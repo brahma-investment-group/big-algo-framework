5cc4eb1e2fd147d69fd311de408b647f99ce3637
ClimberMel <mel.pryor@shaw.ca>
2023-02-18 09:25:20 -0800

    Add module for yFinance Data
    Add a module to fetch historical data from Yahoo Finance using yFinance module.

---
+++ YFhist.py
@@ -0,0 +1,12 @@
+from big_algo_framework.data.yf import YFData
+
+ticker = "TSLA"
+hPeriod = "1mo"
+hInterval = "1d"
+
+#hist = YFData(ticker)
+histData = YFData.get_historic_stock_data(ticker, period=hPeriod, interval=hInterval)
+
+#hist = ticker.history(period=hPeriod, interval=hInterval, start=None, end=None)
+
+print(histData)
